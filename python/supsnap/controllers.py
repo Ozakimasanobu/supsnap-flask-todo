e5bc434057bcd39dd52bac2d6d7a90c998ee2f91
masanobuozaki <b1015181@fun.ac.jp>
2017-08-05 16:18:45 +0900

    [add]develop todo-list app

--- python/supsnap/controllers.py
+++ python/supsnap/controllers.py
@@ -1,22 +1,25 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from app import app
 from db import db
-from models import students
+from models import tasks
 
 @app.route('/')
-def show_all():
-    return render_template('show_all.html', students = students.query.all() )
+def index():
+    return render_template('index.html', tasks = tasks.query.all() )
 
-@app.route('/new', methods = ['GET', 'POST'])
+@app.route('/add', methods=['POST'])
 def new():
     if request.method == 'POST':
-        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
-            flash('Please enter all the fields', 'error')
-        else:
-            student = students(request.form['name'], request.form['city'], request.form['addr'], request.form['pin'])
-            
-            db.session.add(student)
-            db.session.commit()
-            flash('Record was successfully added')
-            return redirect(url_for('show_all'))
-    return render_template('new.html')
+        task = tasks(request.form['title'])
+
+        db.session.add(task)
+        db.session.commit()
+    return redirect("/")
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    if request.method == 'POST':
+        task = db.session.query(tasks).filter_by(id=request.form['id']).first()
+        db.session.delete(task)
+        db.session.commit()
+    return redirect("/")
